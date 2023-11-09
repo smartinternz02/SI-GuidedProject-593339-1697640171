f2a6db5c173cbab4e3d0676fb659c43a44e9712b
Sai Akshith Repaka <132274839+akshithrepaka@users.noreply.github.com>
2023-11-09 12:18:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,10 +14,32 @@ def start():
 
 def login():
 
-    application_type = request.form['at'],
-    signal_strength = request.form['ss'],
-    latency = request.form['lt'],
-    required_bandwidth = request.form['rb'],
+    application_type = request.form['at']
+    if(application_type=="bd"):
+        application_type=0
+    elif(application_type=="es"):
+        application_type=1
+    elif(application_type=="fd"):
+        application_type=2
+    elif(application_type=="it"):
+        application_type=3
+    elif(application_type=="og"):
+        application_type=4
+    elif(application_type=="str"):
+        application_type=5
+    elif(application_type=="vc"):
+        application_type=6
+    elif(application_type=="vs"):
+        application_type=7
+    elif(application_type=="vpc"):
+        application_type=8
+    elif(application_type=="vic"):
+        application_type=9
+    elif(application_type=="wb"):
+        application_type=10
+    signal_strength = request.form['ss']
+    latency = request.form['lt']
+    required_bandwidth = request.form['rb']
     allocated_bandwidth = request.form['ab']
 
     t = [[float(application_type), float(signal_strength), float(latency), float(required_bandwidth), float(allocated_bandwidth)]]
